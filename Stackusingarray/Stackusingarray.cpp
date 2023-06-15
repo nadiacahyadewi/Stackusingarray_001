25d56f052c022780015f796cc49400d6b37935e3
nadiacahyadewi <nadiacahyadewi20@gmail.com>
2023-06-15 10:52:21 +0700

    memeriksa apakah stack kosong atau tidak

--- Stackusingarray/Stackusingarray.cpp
+++ Stackusingarray/Stackusingarray.cpp
@@ -26,7 +26,19 @@ public:
 		cout << element << " ditambahkan(pushed)." << endl;
 
 		return element;
+	}
 
+	void pop() {
+		if (empty()) {
+			cout << "\nStack is empty. Cannot pop." << endl;
+			return;
+		}
+		cout << "\nThe popped element is: " << stack_array[top] << endl;
+		top--;
+	}
+
+	bool empty() {
+		return (top == -1);
 	}
 
 };
